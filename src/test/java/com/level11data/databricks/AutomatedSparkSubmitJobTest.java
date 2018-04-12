eeb80c43d4a1787f10675e6de4897f644621465e
Jason Pohl <jason@databricks.com>
2018-04-12 15:26:36 -0700

    Created test for SparkSubmitJob

--- src/test/java/com/level11data/databricks/AutomatedSparkSubmitJobTest.java
+++ src/test/java/com/level11data/databricks/AutomatedSparkSubmitJobTest.java
@@ -3,8 +3,11 @@ package com.level11data.databricks;
 import com.level11data.databricks.client.DatabricksSession;
 import com.level11data.databricks.cluster.ClusterSpec;
 import com.level11data.databricks.config.DatabricksClientConfiguration;
+import com.level11data.databricks.dbfs.DbfsHelper;
 import com.level11data.databricks.job.AutomatedJarJob;
+import com.level11data.databricks.job.AutomatedSparkSubmitJob;
 import com.level11data.databricks.job.run.AutomatedJarJobRun;
+import com.level11data.databricks.job.run.AutomatedSparkSubmitJobRun;
 import com.level11data.databricks.job.run.RunResultState;
 import com.level11data.databricks.library.JarLibrary;
 import org.junit.Assert;
@@ -16,7 +19,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AutomatedJarJobTest {
+public class AutomatedSparkSubmitJobTest {
 
     public static final String CLIENT_CONFIG_RESOURCE_NAME = "test.properties";
     public static final String SIMPLE_JAR_RESOURCE_NAME = "simple-scala-spark-app_2.11-0.0.1.jar";
@@ -26,7 +29,7 @@ public class AutomatedJarJobTest {
     DatabricksSession _databricks;
     DatabricksClientConfiguration _databricksConfig;
 
-    public AutomatedJarJobTest() throws Exception {
+    public AutomatedSparkSubmitJobTest() throws Exception {
         loadConfigFromResource();
     }
 
@@ -53,7 +56,9 @@ public class AutomatedJarJobTest {
 
         String dbfsPath = "dbfs:/tmp/test/" + uniqueName + "/" + SIMPLE_JAR_RESOURCE_NAME;
         File jarFile = new File(localPath);
-        JarLibrary jarLibrary = _databricks.getJarLibrary(new URI(dbfsPath));
+
+        //Put file onto DBFS
+        _databricks.putDbfsFile(jarFile, dbfsPath);
 
         //create cluster spec
         ClusterSpec clusterSpec = _databricks.createClusterSpec(1)
@@ -63,18 +68,20 @@ public class AutomatedJarJobTest {
 
         //create job
         String jobName = uniqueName;
-        String mainClass = "com.level11data.example.scala.simpleapp.SimpleApp";
         List<String> params = new ArrayList<String>();
+        params.add("--class");
+        params.add("com.level11data.example.scala.simpleapp.SimpleApp");
+        params.add(dbfsPath);
         params.add("hello");
         params.add("world");
 
-        AutomatedJarJob job = _databricks.createJob(jarLibrary, mainClass, jarFile, params)
+        AutomatedSparkSubmitJob job = _databricks.createJob(params)
                 .withName(jobName)
                 .withClusterSpec(clusterSpec)
                 .create();
 
         //run job
-        AutomatedJarJobRun run = job.run();
+        AutomatedSparkSubmitJobRun run = job.run();
 
         while(!run.getRunState().LifeCycleState.isFinal()) {
             Thread.sleep(10000); //sleep 10 seconds
