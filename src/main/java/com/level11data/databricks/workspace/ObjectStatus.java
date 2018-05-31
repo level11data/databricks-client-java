52d6ed716cefac8c96bbfd8d3ce8b729ceac6b9d
Jason Pohl <jason@databricks.com>
2018-05-31 13:33:44 -0700

    Added tests for ScalaNotebook

---
+++ src/main/java/com/level11data/databricks/workspace/ObjectStatus.java
@@ -0,0 +1,7 @@
+package com.level11data.databricks.workspace;
+
+/**
+ * Created by jason on 5/29/18.
+ */
+public class ObjectStatus {
+}
---
+++ src/test/java/com/level11data/databricks/WorkspaceTest.java
@@ -0,0 +1,7 @@
+package com.level11data.databricks;
+
+/**
+ * Created by jason on 5/29/18.
+ */
+public class WorkspaceTest {
+}
