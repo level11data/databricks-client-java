77144c0b4618bcfe5678f9304c828093ffbaeb68
swarnadeepC <swarnadeep.chatterjee@infoworks.io>
2023-07-25 17:29:44 +0530

    Adding storage info for workspace

--- src/main/java/com/level11data/databricks/client/entities/clusters/WorkspaceStorageInfoDTO.java
+++ src/main/java/com/level11data/databricks/client/entities/clusters/WorkspaceStorageInfoDTO.java
@@ -1,32 +1,28 @@
 package com.level11data.databricks.client.entities.clusters;
 
-import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class InitScriptDTO
-{
-    @JsonProperty("dbfs")
-    public DbfsStorageInfoDTO DBFS;
+public class WorkspaceStorageInfoDTO {
+    @JsonProperty("destination")
+    public String Destination;
 
-    @JsonProperty("workspace")
-    public WorkspaceStorageInfoDTO workspace;
-    
     @Override
     public String toString() {
-        final ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         try {
             return ow.writeValueAsString(this);
-        }
-        catch (JsonProcessingException e) {
+        } catch (JsonProcessingException e) {
             return "Could Not Marshal Object to JSON";
         }
     }
